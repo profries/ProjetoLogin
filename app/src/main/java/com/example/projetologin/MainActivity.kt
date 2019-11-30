b8cc1ad971dd847b1b03de322b9aa2a6f789aef8
LUIS HENRIQUE LEAL RIES <lhries@FPAP166748.local>
2019-11-30 10:51:23 -0300

    Acessando strings no Kotlin

--- app/src/main/java/com/example/projetologin/MainActivity.kt
+++ app/src/main/java/com/example/projetologin/MainActivity.kt
@@ -6,23 +6,48 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.example.projetologin.model.Usuario
 
 class MainActivity : AppCompatActivity() {
 
+    var listaUsuarios = ArrayList<Usuario>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        iniciaListaUsuarios()
+
         setContentView(R.layout.activity_main)
+    }
+
+    private fun iniciaListaUsuarios() {
+        listaUsuarios.add(Usuario("usuario1","senha1"))
+        listaUsuarios.add(Usuario("usuario2","senha2"))
+        listaUsuarios.add(Usuario("usuario3","senha3"))
     }
 
     fun realizaLogin(view: View) {
 
         var textoNome = findViewById(R.id.editNome) as EditText
-        var textoSenha = findViewById<EditText>(R.id.editSenha)
+        //var textoSenha = findViewById<EditText>(R.id.editSenha)
+        var textoSenha: EditText = findViewById(R.id.editSenha)
 
+        val nomeString = textoNome.text.toString()
+        val senhaString = textoSenha.text.toString()
 
-        Toast.makeText(this,"Login realizado com sucesso!", Toast.LENGTH_SHORT).show()
-        Log.i("Campo Nome",textoNome.text.toString())
-        Log.i("Campo Senha",textoSenha.text.toString())
+        Log.i("Campo Nome", nomeString)
+        Log.i("Campo Senha",senhaString)
+
+        if(listaUsuarios.contains(Usuario(nomeString,senhaString))){
+            Toast.makeText(this,
+                this.getString(R.string.msg_ok_login), Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this,
+                this.getString(R.string.msg_error_login), Toast.LENGTH_SHORT).show()
+        }
+
+        textoSenha.setText("")
+
 
     }
 
